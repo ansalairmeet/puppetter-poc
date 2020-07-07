d7488608d2af630ac681f65d5024f97783c31061
Geek <anas.ansal10@gmail.com>
2020-07-07 11:51:34 +0100

    Adding demo

--- src/main.js
+++ src/main.js
@@ -3,99 +3,84 @@ const url = 'https://www.youtube.com/watch?v=dffci3_IV64';
 const source_tab = 'selected-tab';
 const width = 1080;
 const height = 1080;
-const PUPPETEER_CHROME_PATH = "/usr/bin/google-chrome-stable";
+const PUPPETEER_CHROME_PATH = '/usr/bin/google-chrome-stable';
 const bootstrap = async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: [
-      '--enable-usermedia-screen-capturing',
-      '--allow-http-screen-capture',
-      '--no-sandbox',
-      '--auto-select-desktop-capture-source=${source_tab}',
-      '--disable-setuid-sandbox',
-      '--disable-web-security',
-      '--use-gl=egl',
-      '--window-size=${width},${height}',
-    ],
-    executablePath: PUPPETEER_CHROME_PATH // set chrome app
-  });
+  const browser = await puppeteer
+    .launch({
+      headless: false,
+      defaultViewport: null,
+      args: [
+        '--enable-usermedia-screen-capturing',
+        '--allow-http-screen-capture',
+        '--auto-select-desktop-capture-source=pickme',
+        '--enable-experimental-web-platform-features',
+        '--disable-setuid-sandbox',
+        '--disable-web-security',
+        '--use-gl=egl',
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--start-maximized',
+      ],
+    });
 
   const page = await browser.newPage();
-  await page.goto( url , {waitUntil: 'networkidle2'});
-
-  const context = browser.defaultBrowserContext();
-  context.clearPermissionOverrides();
-  context.overridePermissions("https://youtube.com", []) ;// <--- don't know what parameter to use
-
-  let dimension = {
-    width: width,
-    height: height,
-  };
-
-  page.evaluate(async (dimension)=>{
-
-    console.log = async (message) => {
-      const logUrl = `http://localhost:5000`;
-      await fetch(logUrl + `/log?q=${message}`, {method: 'GET'})
-    }
-
-    try {
-      navigator.mediaDevices.getDisplayMedia({
-        audio: false,
-        video: {
-          mandatory: {
-            chromeMediaSource: 'screen',
-            maxWidth: 1080,
-            maxHeight: 1080,
-            minFrameRate: 30
-          },
-        }
-      }, stream => {
-        var chunks = [];
-        console.log(` Got Stream `);
-        recorder = new MediaRecorder(stream, {
-          videoBitsPerSecond: 5000000,
-          ignoreMutedMedia: true,
-          mimeType: 'video/webm'
-        });
-
-        console.log(`MediaRecorder created`);
-
-        recorder.ondataavailable = (event) => {
-          console.log(`Recieved event size = ${event.data.size}`)
-          if (event.data.size > 0) {
-            chunks.push(event.data);
-          }
-        };
-
-        recorder.onstop = () => {
-          var superBuffer = new Blob(chunks, {
-            type: 'video/webm'
-          });
-          console.log(`SuperBuffer Size = ${superBuffer.size}`);
-        }
-
-        recorder.start();
-        setTimeout(() => {
-          recorder.stop();
-          console.log(`Stopping Recording`);
-        }, 10000)
-
-      }, error => {
-        console.log(`Unable to get user media ${error}`)
-      })
-      console.log(`Initialized window navigator success`);
-    }catch (e) {
-      console.log(`Exception occurred with initialization ${e}`)
-    }
-
+  page.setViewport({
+    height: 1920,
+    width: 1080,
   });
 
+  await page.goto(url, { waitUntil: 'networkidle2' });
+  await page.waitFor(2000);
+
+  try {
+    await page.evaluate(async () => {
+      document.title = 'pickme';
+
+      console.log = async (message) => {
+        const logUrl = `http://localhost:5000`;
+        await fetch(logUrl + `/log?q=${message}`, { method: 'GET' });
+      };
+
+      const stream = await navigator.mediaDevices.getDisplayMedia({
+        audio: false,
+        video: true,
+      });
+      const recorder = new MediaRecorder(stream, {
+        videoBitsPerSecond: 5000000,
+        ignoreMutedMedia: true,
+        mimeType: 'video/webm',
+      });
+      const chunks = [];
+      recorder.ondataavailable = (event) => {
+        console.log(`Recieved event size = ${event.data.size}`);
+        if (event.data.size > 0) {
+          chunks.push(event.data);
+        }
+      };
+      recorder.onstop = () => {
+        const superBuffer = new Blob(chunks, {
+          type: 'video/webm',
+        });
+        console.log(`SuperBuffer Size = ${superBuffer.size}`);
+      };
+      recorder.start();
+      setTimeout(() => {
+        recorder.stop();
+        console.log(`Stopping Recording`);
+      }, 10000);
+
+    });
+
+    console.log(`Initialized window navigator success`);
+  } catch (e) {
+    console.log(`Exception occurred with initialization ${e}`);
+
+  }
 
   await page.waitFor(15000);
-  console.log(`Force closing it now`)
+  console.log(`Force closing it now`);
   await page.close();
   await browser.close();
-}
+};
 
 bootstrap();
